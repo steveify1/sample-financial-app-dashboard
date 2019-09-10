738cc51b2fd1523feff84900e1df12b1190d405b
Nwakasi Stephen <nwakasistephen@gmail.com>
2019-09-10 18:12:05 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,160 @@
+var user = {
+    username: "ibrahim",
+    password: "12345",
+    balance: 35027.51
+}
+
+
+// Set the Username field on focus whenever the login page is loaded.
+var username = document.querySelector("#username");
+if (username) {
+    username.focus();
+    username.previousElementSibling.classList.add("slide-up");
+}
+
+// Add some google-style input focus event listener
+var loginForm = document.querySelector("#login form");
+if (loginForm) {
+    document.querySelectorAll(".form-control").forEach(i => {
+        i.addEventListener("focus", () => {
+            i.previousElementSibling.classList.add("slide-up");
+        });
+        i.addEventListener("blur", () => {
+            if (i.value === "") i.previousElementSibling.classList.remove("slide-up");
+        });
+
+    });
+}
+
+
+// Our form Validator ES5 Class
+function formValidator() {}
+
+// Checks if the parent of a particular element contains the feedback
+formValidator.prototype.isFeedbackInParent = function (element) {
+    var nextElement = element.nextElementSibling;
+    return nextElement;
+}
+
+// Removes Form feedback
+formValidator.prototype.removeFormFeedback = function (field) {
+    // remove the current message
+    var nextElement = this.isFeedbackInParent(field);
+    if (nextElement) {
+        if (nextElement.classList.contains("form-feedback")) {
+            field.parentElement.removeChild(nextElement);
+        }
+    }
+}
+
+// Returns a feedback to the user. Returns a paragraph element wrapping the feedback message.
+formValidator.prototype.formFeedback = function (field, message) {
+    var p = document.createElement("p");
+    p.className = "error form-feedback";
+    this.feedBackWrapper = p;
+
+    this.feedBackWrapper.innerText = message;
+    // Checking and removing nay former feedback just incase.
+    this.removeFormFeedback(field);
+    // Append the error message to DOM, right below the associated field
+    field.insertAdjacentElement("afterend", this.feedBackWrapper);
+    var that = this;
+    setTimeout(() => {
+        that.removeFormFeedback(field)
+    }, 4000);
+    return this.feedBackWrapper;
+}
+
+// checks if an input field is empty
+formValidator.prototype.areEmpty = function (fields) {
+    var empties = [];
+
+    for (var field = 0; field < fields.length; field++) {
+        if (fields[field].value === "") {
+            empties.push(fields[field]);
+            var msg = fields[field].name + " can not be empty";
+            this.feedBackWrapper = this.formFeedback(fields[field], msg);
+        }
+    }
+    console.log(empties.length === 0);
+    return empties.length !== 0;
+}
+
+// Login Form Validation
+formValidator.prototype.loginValidation = function (e) {
+    e.preventDefault();
+    var username = document.querySelector("#username");
+    var password = document.querySelector("#password");
+    var formTitle = document.querySelector(".form-container .form-title");
+    var btn = document.querySelector(".form input[type=submit]");
+    var usernameVal = username.value;
+
+
+    var empty = this.areEmpty([username, password]);
+    // Checking first that none of the fields are empty
+    if (!empty) {
+        console.log("Password is not empty");
+        if (usernameVal !== user.username) {
+            console.log("Username is not valid");
+            var msg = "We couldn't find a match for '" + usernameVal + "'";
+            this.feedBackWrapper = this.formFeedback(username, msg);
+        } else {
+            console.log("username is valid");
+            if (this.isFeedbackInParent(username)) {
+                console.log("There is already feedback in the DOM")
+                this.removeFormFeedback(username);
+            }
+
+            // Check the password
+            if (password.value !== user.password) {
+                console.log("Password is invalid");
+                this.feedBackWrapper = this.formFeedback(formTitle, "Username and password don't match!");
+            } else {
+                console.log("Password is valid");
+                this.removeFormFeedback(formTitle);
+                // Disable the submit button
+                btn.disbled = true;
+                // Send the form to the backend
+                e.target.submit();
+            }
+        }
+    }
+}
+
+// Validates the Login Form
+var loginForm = document.querySelector("#login form");
+var v = new formValidator();
+// Validating the form
+if (loginForm) {
+    loginForm.addEventListener("submit", function (e) {
+        v.loginValidation(e);
+    });
+}
+
+
+
+// ******* SEARCH FORM FUNCTIONALITIES *********
+var searchForm = document.querySelector(".search-form");
+if (searchForm) {
+    var searchIcon = document.querySelector("#Search");
+    var searchBox = document.querySelector(".search-form .form-control");
+    searchBox.addEventListener("input", function () {
+        if(searchBox.value === "") {
+            searchIcon.classList.remove("active");
+            return;
+        }
+        searchIcon.classList.add("active");
+    });
+}
+
+var bal = document.querySelector(".balance");
+if(bal) {
+    bal.textContent  = user.balance.toLocaleString("en-uk", {
+        style: "currency",
+        currency: "GBP",
+
+    });
+
+    
+}
+
